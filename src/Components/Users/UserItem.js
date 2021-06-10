7a81b6ea38d60a6e565ba6e971bb57bcc8e31136
ggrossman618 <ggrossman@colgate.edu>
2021-06-10 18:03:42 -0400

    a couple quick adjustments and corrections, username/age still doesn't appear

--- src/Components/Users/UserItem.js
+++ src/Components/Users/UserItem.js
@@ -1,12 +1,12 @@
 import React from "react";
 
 function UserItem(props) {
-    console.log(props.name + "<-- should be username");
+    console.log(props.username + " <-- should be username");
     return (
-    <li>
-      <div>{props.username}</div>
-      <div>{props.age}</div>
-    </li>
+    <div>
+      username: {props.username}
+      age: {props.age}
+    </div>
   );
 }
 
