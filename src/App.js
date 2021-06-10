6952ad679f963b808ff2109c6165dbed9c00252d
ggrossman618 <ggrossman@colgate.edu>
2021-06-10 15:36:55 -0400

    workinng on addingn newUser to master user list, **not yet functional**

--- src/App.js
+++ src/App.js
@@ -1,21 +1,19 @@
 import React, { useState } from "react";
-import Users from './Components/Users/Users'
+import Users from "./Components/Users/Users";
 import AddUser from "./Components/Users/AddUser.js";
 import UserItem from "./Components/Users/UserItem";
 
-const dummyUsers = [
-  <UserItem
-  username = 'ben'
-  age = '17'
-  />
-];
+const dummyUsers = [<UserItem username="ben" age="17" />];
 
 function App() {
   const [users, setUsers] = useState(dummyUsers);
 
   function addUserHandler(user) {
     setUsers((prevUsers) => {
-      return [user, ...prevUsers];
+      return [
+        <UserItem username={user.username} age={user.age} />,
+        ...prevUsers,
+      ];
     });
     console.log(users.toString());
   }
